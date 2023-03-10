578d8383830f68661837f70fd8179077867c9ba1
cameliabenavides10 <cameliabenavides@gmail.com>
2023-03-09 20:29:03 -0600

    login, user model, login routing

---
+++ server/seeders/seed.js
@@ -0,0 +1,30 @@
+const db = require('../config/connection');
+const { User } = require('../models');
+const userSeeds = require('./userSeeds.json');
+const thoughtSeeds = require('./thoughtSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await User.deleteMany({});
+
+    await User.create(userSeeds);
+
+    for (let i = 0; i < thoughtSeeds.length; i++) {
+    //    IM CONFUSED ABOUT THIS PART
+      const user = await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        {
+          $addToSet: {
+            thoughts: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
