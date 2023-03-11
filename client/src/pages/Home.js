ae3e478dc223b8a175edd39dea6e6cb009214070
Crothos <s.g.totten@gmail.com>
2023-03-11 13:48:25 -0600

    imported Link to Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-
+import { Link } from 'react-router-dom';
 // import ThoughtList from '../components/ThoughtList';
 // import ThoughtForm from '../components/ThoughtForm';
 
